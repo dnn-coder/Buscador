ab679073a8239f7c2619bfb9d909fb9b346dbfb0
Danny Vivas <64178841+dnn-coder@users.noreply.github.com>
2021-05-21 09:39:25 -0500

    repoappone

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import searchGif from './servicios/searchGif';
+import Gif from './components/Gif';
+import Search from './components/Search';
+import Ejemplo from './ejemplos'
 
 function App() {
+  const [currentGif, setCurrentGif] = useState({
+    images: {
+      original: {
+        url: ''
+      }
+    }
+  });
+  const [queryTerm, setQueryTerm] = useState('puppies');
+  useEffect(() => {
+    searchGif(queryTerm).then(data => {
+      setCurrentGif(data.data[0]);
+    })
+  }, [queryTerm])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Search changeQuery={setQueryTerm}/>
+        <Gif url={currentGif.images.original.url}/>
+        <Ejemplo />
       </header>
     </div>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+const Search = ({changeQuery}) => {
+    const [value, setValue] = useState('');
+    return <div style={{marginBottom: 16}}>
+        <input 
+        value={value} 
+        onInput={ e => {
+            const text = e.target.value
+            setValue(text)
+            console.log(text)
+            }}/>
+        <button onClick={()=> {
+            changeQuery(value)
+         }}
+        >
+            {' '}
+            Search{' '}
+            </button>
+        </div>
+}
+
+export default Search;
